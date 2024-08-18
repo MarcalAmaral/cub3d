5f1abd12207b6dd6e569c3fbbf8585cb3765b2fb
Marcal Yokogawa do amaral campos <myokogaw@c1r2p4.42sp.org.br>
2024-08-18 16:15:09 -0300

    movements and textures

--- src/objs/main.d
+++
@@ -1,4 +0,0 @@
-objs/main.o: main.c hook_move/hook.h ../includes/data.h \
-  ../lib/MLX42/include/MLX42/MLX42.h parse/parse.h \
-  draw_image/draw_image.h ../lib/lib_get_print/includes/libft.h \
-  raycasting/ray_casting.h ../includes/defines.h utils/utils.h
--- src/objs/raycasting/ray_casting.d
+++
@@ -1,4 +0,0 @@
-objs/raycasting/ray_casting.o: raycasting/ray_casting.c \
-  ../lib/lib_get_print/includes/libft.h ../includes/data.h \
-  ../lib/MLX42/include/MLX42/MLX42.h raycasting/ray_casting.h \
-  raycasting/dda.h ../includes/defines.h utils/utils.h
